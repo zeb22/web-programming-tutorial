50d00ae4eb8408dbab44fc635aead211d2cc0132
Seb <zeb.2202@gmail.com>
2016-06-11 11:42:34 +0300

    cu format JSON

--- js/agenda.js
+++ js/agenda.js
@@ -2,23 +2,22 @@ console.debug('loading agenda');
 
 
 function getRow(person) {
-    var firstname = person[2];
-    var lastname = person[1];
-    var position = person[0];
-    var phone = person[3];
+    var firstname = person.firstname;
+    var lastname = person.lastname;
+    var position = person.position;
+    var phone = person.phone;
     var row = '<tr><td>' + position + '</td><td>' + lastname + '</td><td>' + firstname + '</td><td>' + phone + '</td></tr>';
 
     return row;
 }
 
 var contacts = [
-    ['1.', 'Ionescu', 'Matei', '0747001'],
-    ['2.', 'Vasilescu', 'Andrei', '0747001'],
-    ['3.', 'Cotoara', 'Sebi', '0747061'],
-    ['4.', 'Matei', 'Oana', '0747062']//,
-    // ['Manu'],
-    // ['Oana']
-];
+    {position: '1.', lastname: 'Ionescu', firstname: 'Matei', phone: '0747001'},
+    {position: '2.', lastname: 'Vasilescu', firstname: 'Andrei', phone: '0747002'},
+    {position: '3.', lastname: 'Cotoara', firstname: 'Sebi', phone: '0747061'},
+    {position: '4.', lastname: 'Matei', firstname: 'Oana', phone: '0747062'},
+    {position: '5.', lastname: 'Caruntu',firstname: 'Manu', phone: '0747065'}
+]; //array cu JSON
 
 for (var i = 0; i < contacts.length; i++) {
     var person = contacts[i];
