ecf12248c6eab83cbc52901c0d33bcae53de3476
Seb <zeb.2202@gmail.com>
2016-06-11 11:30:59 +0300

    tabel cu array de contacte

--- js/agenda.js
+++ js/agenda.js
@@ -1,5 +1,26 @@
 console.debug('loading agenda');
 
-var row = '<tr><td class="poz">1.</td><td>Georgescu</td><td>Mihai</td><td>0744258</td></tr>';
 
-$('#agenda tbody').append(row);
+function getRow(person) {
+    var firstname = person[2];
+    var lastname = person[1];
+    var position = person[0];
+    var phone = person[3];
+    var row = '<tr><td>' + position + '</td><td>' + lastname + '</td><td>' + firstname + '</td><td>' + phone + '</td></tr>';
+
+    return row;
+}
+
+var contacts = [
+    ['1.', 'Ionescu', 'Matei', '0747001'],
+    ['2.', 'Vasilescu', 'Andrei', '0747001'],
+    ['3.', 'Cotoara', 'Sebi', '0747061'],
+    ['4.', 'Matei', 'Oana', '0747062']//,
+    // ['Manu'],
+    // ['Oana']
+];
+
+for (var i = 0; i < contacts.length; i++) {
+    var person = contacts[i];
+    $('#agenda tbody').append(getRow(person));
+}
