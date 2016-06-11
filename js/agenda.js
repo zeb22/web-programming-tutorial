4a89b465f7e48854616b6e1365f59d7f26b21209
Seb <zeb.2202@gmail.com>
2016-06-11 12:37:33 +0300

    adaugat ajax si rescris metoda

--- js/agenda.js
+++ js/agenda.js
@@ -2,24 +2,32 @@ console.debug('loading agenda');
 
 
 function getRow(person) {
-    var firstname = person.firstname;
-    var lastname = person.lastname;
-    var position = person.position;
-    var phone = person.phone;
-    var row = '<tr><td>' + position + '</td><td>' + lastname + '</td><td>' + firstname + '</td><td>' + phone + '</td></tr>';
+
+    var row = '<tr>' +
+        '<td>' + person.position + '</td>'+
+        '<td>' + person.lastname + '</td>'+
+        '<td>' + person.firstname + '</td>'+'' +
+        '<td>' + person.phone + '</td>'+
+        '<td><button>x</button></td>'+
+        '</tr>';
 
     return row;
 }
 
-var contacts = [
-    {position: '1.', lastname: 'Ionescu', firstname: 'Matei', phone: '0747001'},
-    {position: '2.', lastname: 'Vasilescu', firstname: 'Andrei', phone: '0747002'},
-    {position: '3.', lastname: 'Cotoara', firstname: 'Sebi', phone: '0747061'},
-    {position: '4.', lastname: 'Matei', firstname: 'Oana', phone: '0747062'},
-    {position: '5.', lastname: 'Caruntu',firstname: 'Manu', phone: '0747065'}
-]; //array cu JSON
+$.ajax({
+    url: "js/mocks/load-contacts.json"
+}).done(function(contacts) {
+    console.debug("ajax done", contacts);
+    showContacts(contacts);
+});
 
-for (var i = 0; i < contacts.length; i++) {
-    var person = contacts[i];
-    $('#agenda tbody').append(getRow(person));
+console.debug('after ajax');
+
+function showContacts(contacts) {
+    for (var i = 0; i < contacts.length; i++) {
+        var person = contacts[i];
+        $('#agenda tbody').append(getRow(person));
+    }
+
 }
+
