b3ce6718b2edda573822d757d82f803b966b8372
Seb <zeb.2202@gmail.com>
2016-06-13 13:45:26 +0300

    la final lab

--- js/agenda.js
+++ js/agenda.js
@@ -1,4 +1,4 @@
-console.debug('loading agenda');
+console.debug('1) loading agenda');
 
 
 function getRow(person) {
@@ -8,7 +8,10 @@ function getRow(person) {
         '<td>' + person.lastname + '</td>'+
         '<td>' + person.firstname + '</td>'+'' +
         '<td>' + person.phone + '</td>'+
-        '<td><button data-id="'+ person.id +'">REMOVE</button></td>'+
+        '<td>'+
+            '<button data-id="'+ person.id +'">EDIT</button>'+ ' ' +
+            '<button data-id="'+ person.id +'">REMOVE</button>'+
+        '</td>'+
         '</tr>';
 
     return row;
@@ -17,13 +20,27 @@ function getRow(person) {
 $.ajax({
     url: "js/mocks/load-contacts.json"
 }).done(function(contacts) {
-    console.debug("ajax done", contacts);
+    console.debug("2) ajax done", contacts);
     showContacts(contacts);
 });
 
-console.debug('after ajax');
+function removeContact(id) {
+    $.ajax({
+    url: "js/mocks/remove-contact.json",
+    //type: 'DELETE',
+    type: 'POST',
+    data: {
+        id: id
+    }
+}).done(function(contacts) {
+    showContacts(contacts);
+});
+}
+
+console.debug('3) after ajax');
 
 function showContacts(contacts) {
+    $('#agenda tbody').html('');
     for (var i = 0; i < contacts.length; i++) {
         var persoana = contacts[i];
         $('#agenda tbody').append(getRow(persoana));
@@ -33,5 +50,6 @@ function showContacts(contacts) {
 $('#agenda').on('click', 'button', function () {
     var id = $(this).data('id');
     console.info('remove this contact', this, id);
+    removeContact(id);
 });
 
